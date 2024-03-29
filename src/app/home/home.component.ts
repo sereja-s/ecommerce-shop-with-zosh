ba0c7b56dbff7c11af7c0d5a8b1da49cb7a35dfd
sereja-s <serejasuvorov1979@gmail.com>
2024-03-29 13:50:22 +0300

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { gounsPage1 } from 'src/Data/Gouns/gouns';
+import { menJeans } from 'src/Data/Men/men_jeans';
+import { mens_kurta } from 'src/Data/Men/men_kurta';
+import { lehngacholiPage2 } from 'src/Data/Saree/lenghaCholiPage2';
+import { mensShoesPage1 } from 'src/Data/shoes';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+
+  theMenJeans:any;
+  theWomenGounds:any;
+  theLenghaCholi:any;
+  theKurta:any;
+  theMenShoes:any;
+
+
+	ngOnInit() {
+	  // установим ограничение количества вывода товаров: 5
+   this.theMenJeans = menJeans.slice(0,5);
+   this.theWomenGounds = gounsPage1.slice(0,5);
+   this.theLenghaCholi = lehngacholiPage2.slice(0,5);
+   this.theKurta = mens_kurta.slice(0,5);
+   this.theMenShoes = mensShoesPage1.slice(0,5);
+  }
+
+}
